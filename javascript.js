79bf15997b468fa7dfd0a02b58f172274d5222b3
Mudok-Pro <195640531+Mudok-Pro@users.noreply.github.com>
2025-02-01 13:43:13 +0100

    i added the human choice function and updated the the cumputer function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,20 @@
-function getComputerReturn (){
+
+humanScore =0 ;
+computerScore = 0;
+
+
+function getComputerChoice (){
    const choices = ["Rock", "Paper", "Scissor"];
   const randomIndex = Math.floor(Math.random() * choices.length );
   return choices[randomIndex];
 }
-console.log(getComputerReturn());
+console.log(getComputerChoice());
+
+function getHumanChoice (){
+    let humanChoice= prompt("What is Your Choice? ");
+  
+    return humanChoice;
+}
+console.log(getHumanChoice())
+
+
